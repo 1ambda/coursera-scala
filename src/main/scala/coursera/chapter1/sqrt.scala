6860f4cdb1191092bf6d795c12864f27b37685bb
1ambda <1amb4a@gmail.com>
2014-09-26 11:35:38 +0900

    pdate sqrt.scala

--- src/main/scala/coursera/chapter1/sqrt.scala
+++ src/main/scala/coursera/chapter1/sqrt.scala
@@ -24,10 +24,8 @@ object Math{
     if (n == 0) 1 else n * factorial(n-1);
 
   def tailFactorial(n: Int) = {
-    
     def loop(acc: Int, n: Int): Int =
       if (n == 0) acc else loop(acc * n, n-1)
-
     loop(1, n);
   }
 }
